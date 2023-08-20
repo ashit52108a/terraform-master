2780f1c980142e940668e4d0e082c60090f2b1bd
ashit52108a <ashit52108a@gmail.com>
2023-08-20 21:24:09 +0100

    v1 v1.1

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,11 +1,3 @@
-provider "aws" {
-    version = "5.13.1"
-    access_key = "AKIAYDCWZIA6TAUDNA6Z"
-    secret_key = "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"
-    region     = "us-west-2"
-}
-
-
 resource "aws_instance" "MyFirstInstnace" {
   count         = 3
   ami           = "ami-0688ba7eeeeefe3cd"
---
+++ Start With Terraform Basics/credential.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    version = "5.13.1"
+    access_key = "AKIAYDCWZIA6TAUDNA6Z"
+    secret_key = "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"
+    region     = "us-west-2"
+}
