811e2c3abbfc554d572b35f8574ad47c82d4f88c
ashit52108a <ashit52108a@gmail.com>
2023-08-21 00:12:58 +0100

    v4

--- Start With Terraform Basics/main.tf
+++ Start With Terraform Basics/main.tf
@@ -8,5 +8,23 @@ terraform {
   }
 }
 
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+resource "aws_instance" "example_instance" {
+  count         = var.instance_count
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI
+  instance_type = "t2.micro"
+  
+
+  tags = {
+    Name = "ExampleInstance-${count.index}"
+  }
+}
+
+
 
 
---
+++ Start With Terraform Basics/variable.tf
@@ -0,0 +1,18 @@
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+}
+
+
+}
