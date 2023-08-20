876cb2abe3a6e4b05c06a638c1381170ba2faf20
ashit52108a <ashit52108a@gmail.com>
2023-08-20 22:49:34 +0100

    V3

--- Start With Terraform Basics/credential.tf
+++ Start With Terraform Basics/credential.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    version = "5.13.1"
+    version = "3.53.0"
     access_key = "AKIAYDCWZIA6TAUDNA6Z"
     secret_key = "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"
     region     = "us-west-2"
