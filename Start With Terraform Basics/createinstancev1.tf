23510470086f051d5abf082d8c440a2f85d411fe
ashit52108a <ashit52108a@gmail.com>
2023-08-20 23:21:59 +0100

    v2

--- Start With Terraform Basics/createinstancev1.tf
+++ Start With Terraform Basics/createinstancev1.tf
@@ -1,9 +1,5 @@
 resource "aws_instance" "MyFirstInstnace" {
-  count         = 3
   ami           = "ami-0688ba7eeeeefe3cd"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "demoinstnce-${count.index}"
-  }
 }
+ 
