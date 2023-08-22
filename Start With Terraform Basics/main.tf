c0d68f41334aa1ee6ef2344841a238694dcada7f
ashit52108a <ashit52108a@gmail.com>
2023-08-22 23:19:02 +0100

    22 Aug

--- Start With Terraform Basics/main.tf
+++ Start With Terraform Basics/main.tf
@@ -1,30 +1,11 @@
-
 terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.13.0"
+      version = "5.13.1"
     }
   }
 }
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
-}
-
-resource "aws_instance" "example_instance" {
-  count         = var.instance_count
-  ami           = "ami-0688ba7eeeeefe3cd"  # Replace with your desired AMI
-  instance_type = "t2.micro"
-  
-
-  tags = {
-    Name = "ExampleInstance-${count.index}"
-  }
-}
-
 
 
 
---
+++ Start With Terraform Basics/provider.tf
@@ -0,0 +1,3 @@
+provider "aws" {
+  region = "us-west-2"  # Change this to your desired region
+}
