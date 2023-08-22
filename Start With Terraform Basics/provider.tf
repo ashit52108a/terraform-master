565f777b7f3c4f7b1f91217b1fbfbe7d1f89e173
ashit52108a <ashit52108a@gmail.com>
2023-08-23 00:36:34 +0100

    1

--- Start With Terraform Basics/provider.tf
+++ Start With Terraform Basics/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region        = "${var.AWS_REGION}"
-  access_key    = "${var.AWS_ACCESS_KEY}"
-  secret_key    = "${var.AWS_SECRET_KEY}"
+  region        = var.region
+  access_key    = var.access_key
+  secret_key    = var.secret_key
 }
--- Start With Terraform Basics/variables.tf
+++ Start With Terraform Basics/variables.tf
@@ -1,6 +1,8 @@
-AWS_REGION{
+variable "region" {
     type = string 
     default = "us-west-2"
 }
-ACCESS_KEY{}
-AWS_SECRET_KEY{}
+
+variable "access_key" {}
+
+variable "secret_key" {}
