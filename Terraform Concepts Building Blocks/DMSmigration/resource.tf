c6bee6d45ec939acbe369170fcbd64090abb0fdc
ashit52108a <ashit52108a@gmail.com>
2023-09-25 18:58:00 +0100

    v3

--- Terraform Concepts Building Blocks/DMSmigration/resource.tf
+++ Terraform Concepts Building Blocks/DMSmigration/resource.tf
@@ -1,6 +1,6 @@
 # Create the VPC
 resource "aws_vpc" "dms_vpc" {
-  cidr_block           = "10.0.1.0/24"
+  cidr_block           = "10.0.1.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
